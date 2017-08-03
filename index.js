0aee77c3a907f1988058d5e047b6835434705b7a
Aaron Bernstein <aaron@stitchmedia.ca>
2017-08-03 16:08:52 -0400

    log path to phantomjs

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
+var fs = require('fs')
 var path = require('path')
 var phantomjs = require('phantomjs-prebuilt')
+
+var binPath = phantomjs.path
+if (fs.existsSync(binPath) && path.isAbsolute(binPath) && binPath.indexOf('snapshot') < 0) {
+   console.log('phantomjs is installed at ' + binPath)
+}
+else {
+    console.error('can\'t find phantomjs at ' + binPath)
+}
+
 //using path.dirname(process.execPat) instead of __dirname for zeit/pkg
 var program = phantomjs.exec(path.join(path.dirname(process.execPath), './screenshot.js'), path.dirname(process.execPath), "https://github.com/BernsteinA", "bigshot")
 program.stdout.pipe(process.stdout)
